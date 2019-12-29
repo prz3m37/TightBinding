1c8cef1584484a98814491c5f0b58bcf2f1463cc
prz3m37 <54777041+prz3m37@users.noreply.github.com>
2019-12-29 19:21:28 +0100

    Add files via upload

--- tightbinding_code/tight_binding.sh
+++ tightbinding_code/tight_binding.sh
@@ -1,9 +1,9 @@
 echo "############################################################################ TIGHT_BINDING_CALCULATIONS  ############################################################################"
 echo
 now_start=$(date)
-echo "_____________Program for 5 parametrizations has started at : $now_start"
+echo "_____________Program for parametrization has started at : $now_start"
 echo
 python3 "./main.py" parametrization
 echo
 now_end=$(date)
-echo "_____________Program for 5 parametrizations has ended at : $now_end"
+echo "_____________Program for parametrization has ended at : $now_end"
